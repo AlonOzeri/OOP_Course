f13b2750fe2e341497ec1070c716dec15928a3e5
AlonOzeri <alonozeri8@gmail.com>
2023-08-31 11:06:48 +0300

    Task 2 - Done

--- Task 2/Main.cpp
+++ Task 2/Main.cpp
@@ -4,19 +4,20 @@
 
 int main()
 {
-	// Declare 4 points
-    cPoint c1(2, 4), c2(6, 4), c3(6, 0), c4(2, 0);
+	//Declare 4 points
+    cPoint p1(2, 4), p2(6, 4), p3(6, 0), p4(2, 0);
 	cout << "Points are: " << endl;
-	c1.printPoint();
+	p1.printPoint();
 	cout << endl;
-	c2.printPoint();
+	p2.printPoint();
 	cout << endl;
-	c3.printPoint();
+	p3.printPoint();
 	cout << endl;
-	c4.printPoint();
+	p4.printPoint();
 	cout << endl << endl;
-	// Declare a rectangle of those 4 points
-	cRect rect1(c1,c2, c3, c4);
+
+	//Declare a rectangle with those 4 points
+	cRect rect1(p1, p2, p3, p4);
 	cout << "First rectangle is: " << endl;
 	rect1.printRect();
 	cout << "Point C in the rectangle is: ";
@@ -27,19 +28,18 @@ int main()
 	cout << endl;
 	cout << "First rectangle area is: " << rect1.calcArea() << endl << endl;
 
-	// Now we change the points and declare a new Rectangle
-	c1.setY(c1.getY() + 1);
-	c2.setY(c2.getY() + 1);
-
-	cRect rect2(c1, c2, c3, c4);
+	//Change the points and declare a new Rectangle
+	p1.setY(p1.getY() + 1);
+	p2.setY(p2.getY() + 1);
+	cRect rect2(p1, p2, p3, p4);
 	cout << "Second rectangle is: " << endl;
 	rect2.printRect();
 	cout << "Second rectangle area is: " << rect2.calcArea() << endl << endl;
-	c2.setX(c2.getX() - 1);
-	c3.setX(c3.getX() - 1);
+	p2.setX(p2.getX() - 1);
+	p3.setX(p3.getX() - 1);
 	// Now instead of changing the points, we will change the rectangle
-	rect2.setPoint(c2, 1);
-	rect2.setPoint(c3, 2);
+	rect2.setPoint(p2, 1);
+	rect2.setPoint(p3, 2);
 	cout << "Second rectangle after change is: " << endl;
 	rect2.printRect();
 	cout << "Second rectangle area is: " << rect2.calcArea() << endl;
